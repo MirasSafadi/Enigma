f3e34d2e22c769faa7f2478d17232bb443f06446
MirasSafadi <45172450+MirasSafadi@users.noreply.github.com>
2022-11-12 12:37:50 +0200

    Added more models and logic

--- src/main/java/com/mirass/enigma/models/PlugBoard.java
+++ src/main/java/com/mirass/enigma/models/PlugBoard.java
@@ -1,22 +1,62 @@
 package com.mirass.enigma.models;
 
 import com.mirass.enigma.utils.Constants;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashMap;
 
+@Getter
+@Setter
 public class PlugBoard {
+    /**
+     * The plugs on the plug board.
+     * It is basically a mapping of letters in pairs or to each other.
+     */
     private HashMap<Character,Character> plugs;
 
-    public PlugBoard(HashMap<Character,Character> customPlugBoard){
-        if(customPlugBoard == null || customPlugBoard.isEmpty()){
-            this.plugs = new HashMap<>();
-            initDefaultPlugBoard(this.plugs);
-        } else{
-            this.plugs = customPlugBoard;
+    /**
+     * Initialize the plug board (in reality this is like plugging in the cables)
+     *
+     * @param cables
+     */
+    public PlugBoard(HashMap<Character,Character> cables){
+        this.plugs = new HashMap<>();
+        initDefaultPlugBoard(this.plugs);
+        if(cables == null || cables.isEmpty()) {
+            return;
+        }
+        if(isCustomPlugBoardValid(cables)){
+            configurePlugBoard(cables);
         }
     }
 
+    /**
+     *
+     * @param cables
+     */
+    private void configurePlugBoard(HashMap<Character,Character> cables){
+        this.plugs.replaceAll((key,oldValue)->cables.get(key));
+    }
+
+    /**
+     *
+     * @param customPlugBoard
+     * @return
+     */
+    private boolean isCustomPlugBoardValid(HashMap<Character,Character> customPlugBoard){
+        boolean isValid;
+        customPlugBoard.size();
+        isValid = customPlugBoard.size() <= Constants.MAX_PLUGS_COUNT;
+        return isValid;
+    }
+
+    /**
+     *
+     * @param plugBoard
+     */
     private void initDefaultPlugBoard(HashMap<Character,Character> plugBoard){
-        for(char ch: Constants.chars){
+        for(char ch: Constants.CHARS){
             plugBoard.put(ch,ch);
         }
     }
--- src/main/java/com/mirass/enigma/utils/Constants.java
+++ src/main/java/com/mirass/enigma/utils/Constants.java
@@ -1,6 +1,7 @@
 package com.mirass.enigma.utils;
 
 public class Constants {
-    public static char[] chars = {'A','B','C','D','E','F','G','H','I','J','K',
+    public static final char[] CHARS = {'A','B','C','D','E','F','G','H','I','J','K',
             'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    public static final int MAX_PLUGS_COUNT = 13;
 }
